3b10a00aad178e68284936f835d018e991303e80
rdbckp <ardibackup@gmail.com>
2026-02-19 04:39:37 +0700

    Update device.mk
    Signed-off-by: rdbckp <ardibackup@gmail.com>

--- device.mk
+++ device.mk
@@ -6,3 +6,6 @@
 #
 
 LOCAL_PATH := device/samsung/a02
+# Copy ueventd dari root.zip ke root ramdisk recovery
+PRODUCT_COPY_FILES += \
+    device/samsung/a02/root.zip/system/etc/ueventd.rc:recovery/root/ueventd.rc
